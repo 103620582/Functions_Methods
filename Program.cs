ac9df2f97b5b50e4e032d53086c18f304d63e571
103620582 <103620582@student.swin.edu.au>
2021-05-11 20:25:19 +1000

    Exercise 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FunctionsMethods
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Welcome();
+            NiceDay();
+        
+        }
+
+        public static void Welcome() {
+           Console.WriteLine("Welcome friends.");
+        }
+
+        public static void NiceDay() {
+            Console.WriteLine("Have a nice day!");
+        }
+    
+
+    }
+}
